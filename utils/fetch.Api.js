1c26662725925efa6ced6271593d0155598fbfb0
Matheus Ghignatti <matheusghignatti@gmail.com>
2022-10-27 15:29:56 -0300

    securing the keys

--- utils/fetch.Api.js
+++ utils/fetch.Api.js
@@ -5,7 +5,7 @@ export const baseUrl = "https://bayut.p.rapidapi.com";
 export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
-      "X-RapidAPI-Key": "4e1d7a2947mshf7285688137a9d0p180cd4jsn6e214f573c2e",
+      "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY,
       "X-RapidAPI-Host": "bayut.p.rapidapi.com",
     },
   });
