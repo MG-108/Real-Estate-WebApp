6bf7bf5d417a43537c748095b94cce9ed53b6532
Matheus Ghignatti <matheusghignatti@gmail.com>
2022-10-27 17:10:44 -0300

    1

--- next.config.js
+++ next.config.js
@@ -6,8 +6,7 @@ const nextConfig = {
     domains: ["bayut-production.s3.eu-central-1.amazonaws.com"],
   },
   env: {
-    NEXT_PUBLIC_RAPID_API_KEY:
-      "4e1d7a2947mshf7285688137a9d0p180cd4jsn6e214f573c2e",
+    RAPID_API_KEY: process.env.NEXT_PUBLIC_RAPID_API_KEY,
   },
 };
 
