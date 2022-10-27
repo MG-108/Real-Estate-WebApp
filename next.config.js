0355d81feb317f74e4aefccea3d16e96465d11e9
Matheus Ghignatti <matheusghignatti@gmail.com>
2022-10-27 16:43:59 -0300

    deploy

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
   images: {
     domains: ["bayut-production.s3.eu-central-1.amazonaws.com"],
   },
+  env: {
+    NEXT_PUBLIC_RAPID_API_KEY:
+      "4e1d7a2947mshf7285688137a9d0p180cd4jsn6e214f573c2e",
+  },
 };
 
 module.exports = nextConfig;
